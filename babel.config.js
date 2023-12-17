d94d5a8b69c075d697ff48e3b958c00408bfc566
anhnh153 <anhnh153@viettel.com.vn>
2023-12-17 16:30:35 +0700

    base eslint new 12 2023

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		[
+			require.resolve('babel-plugin-module-resolver'),
+			{
+				root: ['.'],
+				alias: {
+					src: './src',
+					// svg: "./src/assets/svg",
+					// images: "./src/assets/images",
+				},
+			},
+		],
+		'react-native-reanimated/plugin',
+	],
 };
